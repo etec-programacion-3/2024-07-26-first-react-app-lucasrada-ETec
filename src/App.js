b0c2c6e58badd4efab4cb93f8e01b2839206253c
Lucas Rada <l.rada@alumno.etec.um.edu.ar>
2024-08-16 12:54:38 -0300

    Merges a website into the React app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import './styles/Header.css';
+import './styles/Footer.css';
+import './styles/main.css';
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <html>
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@100..900&display=swap" rel="stylesheet"></link>
+      </head>
+      <body>
+        <Header />
+        <main>
+          <div class="musicBanner">
+              <img id="newReleaseImage" class="newReleaseAlbum" src={require("./media/ttpd.jpg")} alt="The Tortured Poets Department" onmouseover="imageFocus(this, 1.04)" onmouseout="imageFocusOut(this)" />
+              <div>
+                  <p id="album" class="album">
+                      The Tortured Poets Department
+                  </p>
+                  <p class="artist">
+                      Taylor Swift
+                  </p>
+                  <div id="releaseDate" class="releaseDate">19/04/2024</div>
+              </div>
+          </div>
+          <div class="audioBanner">
+              <div class="audioProduct">
+                  <img class="productImage" src={require("./media/WH-1000XM4.png")} alt="" onmouseover="imageFocus(this, 1.02)" onmouseout="imageFocusOut(this)" />
+                  <p class="product">WH-1000XM4</p>
+              </div>
+              <div class="audioProduct">
+                  <img class="productImage" src={require("./media/WH-1000XM4.png")} alt="" onmouseover="imageFocus(this, 1.02)" onmouseout="imageFocusOut(this)" />
+                  <p class="product">WH-1000XM4</p>
+              </div>
+          </div>
+        </main>
+        <Footer />
+      </body>
+    </html>
   );
 }
 
